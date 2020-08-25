6afcbfa18af9af6779c94191dea9f890d876d5b0
Oskari Järvinen <ojeinposti@gmail.com>
2020-08-25 22:27:15 +0300

    added cmake list

---
+++ src/CMakeFiles/WIDEO_PELI.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/juuusto/cplusplus/micro-machines-2020-1/src/Grid.cpp" "/home/juuusto/cplusplus/micro-machines-2020-1/src/CMakeFiles/WIDEO_PELI.dir/Grid.cpp.o"
+  "/home/juuusto/cplusplus/micro-machines-2020-1/src/Map.cpp" "/home/juuusto/cplusplus/micro-machines-2020-1/src/CMakeFiles/WIDEO_PELI.dir/Map.cpp.o"
+  "/home/juuusto/cplusplus/micro-machines-2020-1/src/Menu.cpp" "/home/juuusto/cplusplus/micro-machines-2020-1/src/CMakeFiles/WIDEO_PELI.dir/Menu.cpp.o"
+  "/home/juuusto/cplusplus/micro-machines-2020-1/src/boost.cpp" "/home/juuusto/cplusplus/micro-machines-2020-1/src/CMakeFiles/WIDEO_PELI.dir/boost.cpp.o"
+  "/home/juuusto/cplusplus/micro-machines-2020-1/src/main.cpp" "/home/juuusto/cplusplus/micro-machines-2020-1/src/CMakeFiles/WIDEO_PELI.dir/main.cpp.o"
+  "/home/juuusto/cplusplus/micro-machines-2020-1/src/network.cpp" "/home/juuusto/cplusplus/micro-machines-2020-1/src/CMakeFiles/WIDEO_PELI.dir/network.cpp.o"
+  "/home/juuusto/cplusplus/micro-machines-2020-1/src/server.cpp" "/home/juuusto/cplusplus/micro-machines-2020-1/src/CMakeFiles/WIDEO_PELI.dir/server.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/juuusto/SFML-2.5.1/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/CMakeFiles/WIDEO_PELI.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WIDEO_PELI.dir/boost.cpp.o"
+  "CMakeFiles/WIDEO_PELI.dir/Grid.cpp.o"
+  "CMakeFiles/WIDEO_PELI.dir/main.cpp.o"
+  "CMakeFiles/WIDEO_PELI.dir/Map.cpp.o"
+  "CMakeFiles/WIDEO_PELI.dir/Menu.cpp.o"
+  "CMakeFiles/WIDEO_PELI.dir/network.cpp.o"
+  "CMakeFiles/WIDEO_PELI.dir/server.cpp.o"
+  "WIDEO_PELI.pdb"
+  "WIDEO_PELI"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WIDEO_PELI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
