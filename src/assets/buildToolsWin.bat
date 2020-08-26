bcc54b38bdb69611939f148e2c4545bf8d7fb475
Antti Nousiainen <antti.h.nousiainen@aalto.fi>
2020-08-27 01:38:06 +0300

    added server map tool

--- src/assets/buildToolsWin.bat
+++ src/assets/buildToolsWin.bat
@@ -1,6 +1,7 @@
 g++  -c mapMaker.cpp
 g++  mapMaker.o -o mapEdit.exe
-
+g++  -c srvMapMaker.cpp
+g++  srvMapMaker.o -o srvMapEdit.exe
 
 g++  -I"..\SFML-2.5.1\include" -c collisionMaker.cpp
 g++  -L"..\SFML-2.5.1\lib" collisionMaker.o -o tileEdit.exe -lsfml-graphics -lsfml-window -lsfml-system -lsfml-network
