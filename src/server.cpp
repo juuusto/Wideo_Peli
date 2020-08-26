aca155bd3a172e416fc0bf8dfb8f61d56b94e78b
Antti Nousiainen <antti.h.nousiainen@aalto.fi>
2020-08-26 15:37:07 +0300

    added initial ai

--- src/server.cpp
+++ src/server.cpp
@@ -1,10 +1,20 @@
 #include <SFML/Network.hpp>
 #include<iostream>
 #include <ctime>
+#include <math.h>   
+#include <chrono>
 
-
-
-
+struct aiplayerServerData{
+    float x;
+    float y;
+    float r;
+    int type;
+    float aiSpeed;
+    std::time_t connectTimeout;
+    std::vector<std::pair<int,int>> path;
+    int aiTarget;
+    std::string name;
+};
 struct playerServerData{
     int x;
     int y;
@@ -18,10 +28,29 @@ struct projectileData{
     int y;
 };
 
+float calcAIrotDiff(int ox, int oy, int nx, int ny){
+
+        float xdiff = nx-ox;
+        float ydiff = ny-oy;
+
+        if(xdiff == 0){
+            if(ydiff<0) return 90.f;
+            else return 270.f;
+        } else if (xdiff<0){
+            return 180+std::atan (ydiff/xdiff) * 180 / 3.14f;
+
+        } else{
+            return std::atan (ydiff/xdiff) * 180 / 3.14f;
+        }
+}
 
 
 int main()
 {
+
+    std::vector<std::pair<int,int>> aiPath{ std::pair<int,int>(500,300),std::pair<int,int>(800,400),std::pair<int,int>(1900,200)}; 
+    std::vector<std::pair<int,int>> aiPath1{ std::pair<int,int>(600,200),std::pair<int,int>(500,400),std::pair<int,int>(2900,200)};
+
     std::string SERVER_MAP = "map3.map";
     unsigned short inPort = 54010;
     unsigned short outPort = 54000;
@@ -29,12 +58,42 @@ int main()
     socket.bind(inPort) != sf::Socket::Done;
     socket.setBlocking(false);
     std::vector<playerServerData> players;
+    std::vector<aiplayerServerData> aiPlayers;
     std::vector<std::vector<projectileData>> projData;
-
-
     std::time_t currentTime = std::time(nullptr);
+std::chrono::steady_clock::time_point betterTime = std::chrono::steady_clock::now();
+
+
+    // for AI
+    float aiX =aiPath[0].first;
+    float aiY = aiPath[0].second;
+    aiPlayers.push_back(aiplayerServerData{aiX,aiY,0,0,15.f,currentTime, aiPath,0, "ai"});
+    aiX =aiPath1[0].first;
+    aiY = aiPath1[0].second;
+    aiPlayers.push_back(aiplayerServerData{aiX,aiY,0,1,15.f,currentTime, aiPath1,0, "ai1"});
+
+    std::chrono::steady_clock::time_point aiUptdTime = betterTime;
     while(true){
         currentTime = std::time(nullptr);
+        betterTime = std::chrono::steady_clock::now();
+
+
+        if(std::chrono::duration_cast<std::chrono::milliseconds>(betterTime - aiUptdTime).count()>50){
+            aiUptdTime = betterTime;
+            for(int ThisAi=0; ThisAi<aiPlayers.size();ThisAi++){
+                if (abs(aiPlayers[ThisAi].x-  aiPlayers[ThisAi].path[aiPlayers[ThisAi].aiTarget].first)>20 || abs(aiPlayers[ThisAi].y- aiPlayers[ThisAi].path[aiPlayers[ThisAi].aiTarget].second)>20){
+                    aiPlayers[ThisAi].x += aiPlayers[ThisAi].aiSpeed * cos((aiPlayers[ThisAi].r / 180.f) * 3.14f);
+                    aiPlayers[ThisAi].y += aiPlayers[ThisAi].aiSpeed * sin((aiPlayers[ThisAi].r / 180.f) * 3.14f);
+                    aiPlayers[ThisAi].connectTimeout = currentTime;
+                    //std::cout << "X:"<<aiPlayers[ThisAi].x<<"Y:"<<aiPlayers[ThisAi].y<<"R:"<< aiPlayers[ThisAi].r<<std::endl;
+                } else {
+                    aiPlayers[ThisAi].aiTarget++;
+                    aiPlayers[ThisAi].aiTarget = aiPlayers[ThisAi].aiTarget%aiPlayers[ThisAi].path.size();
+                    aiPlayers[ThisAi].r = calcAIrotDiff(aiPlayers[ThisAi].x,aiPlayers[ThisAi].y,aiPlayers[ThisAi].path[aiPlayers[ThisAi].aiTarget].first,aiPlayers[ThisAi].path[aiPlayers[ThisAi].aiTarget].second);
+                }
+            }
+
+        }
 
         sf::IpAddress sender;
         char data[100];
@@ -88,6 +147,12 @@ int main()
             res = "";
             for (playerServerData pd: players){
                 if(currentTime-pd.connectTimeout<2)res += std::to_string(pd.x)+";"+std::to_string(pd.y)+";"+std::to_string(pd.r)+";"+std::to_string(pd.type)+";"+pd.name+";";
+            }
+            for (aiplayerServerData pd: aiPlayers){
+                int ax = pd.x;
+                int ay = pd.y;
+                int ar = pd.r;
+                res += std::to_string(ax)+";"+std::to_string(ay)+";"+std::to_string(ar)+";"+std::to_string(100+pd.type)+";"+pd.name+";";
             }
         } else if (tmp_arr[0]=="UASSETS"){
             std::cout << "Update projectiles... \n"<<std::endl;
